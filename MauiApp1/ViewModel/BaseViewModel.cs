9a1cd8c4e77fbfa6f6ddb34c3c605f945cc0a1b5
elsa.m <manangelsa07@gmail.com>
2024-06-13 23:14:26 +0200

    adding base viewmodel

---
+++ MauiApp1/ViewModel/BaseViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        bool isBusy;
+        string title;
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set {
+                if (isBusy == value) {
+                    return;
+                }
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string Title
+        {
+            get => title;
+            set {
+                title = value;
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+        }
+
+        public bool IsNotBusy => !isBusy;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = null) => 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
