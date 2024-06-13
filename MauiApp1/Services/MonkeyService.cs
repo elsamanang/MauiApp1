5ebd70235d7a615aef1e0d0aa603af23fda7ba51
elsa.m <manangelsa07@gmail.com>
2024-06-13 23:14:08 +0200

    adding service

---
+++ MauiApp1/Services/MonkeyService.cs
@@ -0,0 +1,33 @@
+﻿using MauiApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public class MonkeyService
+    {
+        private List<Monkey>? monkeyList;
+        private HttpClient httpClient;
+
+        public MonkeyService()
+        {
+            httpClient = new HttpClient();
+        }
+        public async Task<List<Monkey>> GetMonkeys()
+        {
+            if (monkeyList.Count is > 0) return monkeyList;
+
+            var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+            if(response.IsSuccessStatusCode)
+            {
+                monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+            }
+
+            return monkeyList;
+        }
+    }
+}
