f3847038bfa08cb0d6ddfc4ca45b3fc8dedb1277
elsa.m <manangelsa07@gmail.com>
2024-06-13 23:14:50 +0200

    update viewmodel

---
+++ MauiApp1/ViewModel/MonkeyViewModel.cs
@@ -0,0 +1,54 @@
+﻿using MauiApp1.Models;
+using MauiApp1.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModel
+{
+    public partial class MonkeyViewModel : BaseViewModel
+    {
+        public ObservableCollection<Monkey> Monkeys { get; set; } = new();
+        private MonkeyService _monkeyService;
+        public Command GetMonkeyCommand { get; }
+
+        public MonkeyViewModel(MonkeyService monkeyService)
+        {
+            Title = "Monkey list";
+            _monkeyService = monkeyService;
+            GetMonkeyCommand = new Command(async () => await GetMonkeyAsync());
+        }
+
+        public async Task GetMonkeyAsync()
+        {
+            if(IsBusy) return;
+
+            try
+            {
+                IsBusy = true;
+
+                var monkeys = await _monkeyService.GetMonkeys();
+
+                if(monkeys.Count != 0) Monkeys.Clear(); 
+
+                foreach(var monkey in monkeys)
+                {
+                    Monkeys.Add(monkey);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"Something wrong : {ex.Message}");
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
